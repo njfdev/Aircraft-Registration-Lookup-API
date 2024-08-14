7f9074ba82c0f15f6eeeb1e9eb5af89fc15bf163
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-14 15:35:45 -0400

    Add Logic for Downloading, Decompressing, and Decoding FAA Aircraft Registration CSV File

--- src/app/api/update_faa_db/route.ts
+++ src/app/api/update_faa_db/route.ts
@@ -1,6 +1,10 @@
 import type { NextRequest } from "next/server";
+import AdmZip from "adm-zip";
+import { Stream } from "stream";
+import csvParser from "csv-parser";
+import faa_sample_data from "../../../sample_faa_data";
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   // verify that the request is coming from the vercel cron job and not a random person
   const authHeader = request.headers.get("authorization");
   if (
@@ -11,6 +15,47 @@ export function GET(request: NextRequest) {
       status: 401,
     });
   }
+
+  // download faa aircraft registration csv files
+  // const zip_download_res = await fetch(
+  //   "https://registry.faa.gov/database/ReleasableAircraft.zip"
+  // );
+  // if (!zip_download_res.ok) {
+  //   return new Response("Internal Server Error", {
+  //     status: 500,
+  //   });
+  // }
+
+  // const zip_object = new AdmZip(
+  //   Buffer.from(await zip_download_res.arrayBuffer())
+  // );
+
+  // const registration_csv = zip_object.getEntry("MASTER.txt");
+  // if (!registration_csv || registration_csv.isDirectory) {
+  //   return new Response("Internal Server Error", {
+  //     status: 500,
+  //   });
+  // }
+  //
+  // let csv_buffer = registration_csv.getData();
+  let csv_buffer = Buffer.from(faa_sample_data);
+
+  // create a readable stream from the csv buffer
+  const readable = new Stream.Readable();
+  readable._read = () => {};
+  readable.push(csv_buffer);
+  readable.push(null);
+
+  const aircraft_registration_data: any[] = [];
+
+  readable
+    .pipe(csvParser())
+    .on("data", (row) => {
+      aircraft_registration_data.push(row);
+    })
+    .on("end", () => {
+      console.log(aircraft_registration_data);
+    });
 
   return Response.json({ success: true });
 }
