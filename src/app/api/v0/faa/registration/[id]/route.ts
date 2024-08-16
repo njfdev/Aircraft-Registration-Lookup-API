a63128cb83f69ef117e9cfe68970e448d136326b
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-16 17:06:14 -0400

    Allow Registration API to Accept N-Number or Mode S Hex Code

--- src/app/api/v0/faa/registration/[id]/route.ts
+++ src/app/api/v0/faa/registration/[id]/route.ts
@@ -3,14 +3,22 @@ import { NextRequest } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params: { n_number } }: { params: { n_number: string } }
+  { params: { id } }: { params: { id: string } }
 ) {
   const prisma = new PrismaClient();
 
+  let idUpper = id.toUpperCase();
+
+  const findParams = idUpper.startsWith("N")
+    ? {
+        n_number: idUpper.slice(1),
+      }
+    : {
+        mode_s_code_hex: idUpper,
+      };
+
   const registration_result = await prisma.faaAircraftRegistration.findUnique({
-    where: {
-      n_number,
-    },
+    where: findParams,
     include: {
       aircraft_info: true,
       engine_info: true,
@@ -20,7 +28,9 @@ export async function GET(
   if (!registration_result) {
     return Response.json({
       error: "not_found",
-      message: "Could not find registration data for the specified N-Number.",
+      message: `Could not find registration data for the specified ${
+        idUpper.startsWith("N") ? "N-Number" : "Mode S Hex Code"
+      }.`,
     });
   }
 
