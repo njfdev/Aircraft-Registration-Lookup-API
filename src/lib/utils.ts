da721a79017de4df1035b1b622175585b354510a
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-16 17:37:49 -0400

    Fix Overwritten Utils by ShadCN UI Init Command

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export default function parse8CharDate(dateString: string): Date | null {
+  const year = Number(dateString.substring(0, 4));
+  const month = Number(dateString.substring(4, 6)) - 1; // Month is 0-indexed in JS Date
+  const day = Number(dateString.substring(6, 8));
+
+  if (isNaN(year) || isNaN(month) || isNaN(day)) {
+    return null;
+  }
+
+  return new Date(year, month, day);
 }
