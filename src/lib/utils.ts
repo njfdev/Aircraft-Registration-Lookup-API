419cc01f0bfb2841d1e539079999ce066e1cdbcf
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-16 17:34:30 -0400

    Initialize ShadCN UI

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,6 @@
-export default function parse8CharDate(dateString: string): Date | null {
-  const year = Number(dateString.substring(0, 4));
-  const month = Number(dateString.substring(4, 6)) - 1; // Month is 0-indexed in JS Date
-  const day = Number(dateString.substring(6, 8));
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-  if (isNaN(year) || isNaN(month) || isNaN(day)) {
-    return null;
-  }
-
-  return new Date(year, month, day);
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
