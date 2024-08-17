85d8f71a2f87e5a9972bf661bbdaaad455499e05
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-08-17 13:24:25 -0400

    Add Documentation for Registration API

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -15,6 +18,7 @@ import {
 } from "@/components/ui/table";
 import { MAX_FREE_REQUESTS_PER_MINUTE } from "@/lib/types";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function DocsPage() {
   return (
@@ -59,11 +63,55 @@ export default function DocsPage() {
         </TableCaption>
       </Table>
       <h2 className="mt-4 text-2xl font-bold">API Routes</h2>
-      <p>The API consists of only 3 endpoints.</p>
+      <p>
+        The API consists of only 3 endpoints. All endpoints listed here must be
+        prefix with &quot;/api/v0&quot;. If you call any endpoint without its
+        parameter, a random one will be returned. All parameters are NOT case
+        sensitive.
+      </p>
       <Accordion type="single" collapsible>
         <AccordionItem value="faa-registration">
           <AccordionTrigger>/faa/registration</AccordionTrigger>
-          <AccordionContent></AccordionContent>
+          <AccordionContent className="flex flex-col gap-2 text-base">
+            <code className="text-primary">
+              https://arla.njf.dev/api/v0/faa/registration/[MODE_S || N_NUMBER]
+            </code>
+            <p>
+              The registration endpoint will return all registration and model
+              information about an aircraft based on a given Mode S or N-Number
+              registration.
+            </p>
+            <p>
+              <code className="text-primary">MODE_S</code> - The Mode S hex code
+              (a.k.a ICAO Address) is a 6 digit hexadecimal number that uniquely
+              identifies an aircraft such as <code>A06D1D</code>.
+            </p>
+            <p>
+              <code className="text-primary">N_NUMBER</code> - The N-Number
+              registration is specific to aircraft registered with the FAA. It
+              is always prefixed with the letter &quot;N&quot;, may not exceed 6
+              characters, and can contains letters or numbers. For example,{" "}
+              <code>N560WN</code>.
+            </p>
+            <h3 className="text-xl font-semibold">Response Object</h3>
+            <pre className="rounded p-4 dark:bg-stone-900 bg-stone-100">
+              &#123;{"\n"}
+              {"  "}&quot;registration&quot;:{" "}
+              <Link
+                href=""
+                className="underline hover:opacity-80 active:opacity-65"
+              >
+                RegistrationObject
+              </Link>
+              {"\n"}
+              &#125;
+            </pre>
+            <h3 className="text-xl font-semibold mt-2">Try Yourself</h3>
+            <TryYourselfAPIBlock
+              endpoint="/faa/registration"
+              parameters={["", "A06D1D", "N560WN"]}
+            />
+          </AccordionContent>
         </AccordionItem>
         <AccordionItem value="aircraft-model">
           <AccordionTrigger>/faa/aircraft_model</AccordionTrigger>
@@ -75,5 +123,52 @@ export default function DocsPage() {
         </AccordionItem>
       </Accordion>
     </main>
+  );
+}
+
+function TryYourselfAPIBlock({
+  endpoint,
+  parameters,
+}: {
+  endpoint: string;
+  parameters: string[];
+}) {
+  const [currentResult, setCurrentResult] = useState("");
+  const [currentApiPath, setCurrentApiPath] = useState("");
+
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="flex gap-1 w-full *:grow">
+        {parameters.map((parameter) => {
+          return (
+            <Button
+              onClick={async () => {
+                const requestPath = `/api/v0${endpoint}/${parameter}`;
+                setCurrentApiPath(requestPath);
+                const apiResult = await fetch(requestPath);
+                setCurrentResult(
+                  JSON.stringify(await apiResult.json()!, undefined, "  ")
+                );
+              }}
+              key={parameter}
+            >
+              {endpoint}
+              {parameter && `/${parameter}`}
+            </Button>
+          );
+        })}
+      </div>
+      <pre className="rounded p-4 dark:bg-stone-900 bg-stone-100">
+        {currentApiPath ? (
+          <span className="text-primary">
+            https://arla.njf.dev{currentApiPath}
+            {"\n"}
+          </span>
+        ) : (
+          "{}"
+        )}
+        {currentResult}
+      </pre>
+    </div>
   );
 }
